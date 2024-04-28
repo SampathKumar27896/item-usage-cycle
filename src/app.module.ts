1e895f98189ada072ee80b3f224749bc780bc5ef
Sampathkumar27896 <sampathkumar27896@hotmail.com>
2024-04-28 11:30:01 +0530

    Added a middleware for all the routes that prints the timestamp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
-
+import { LoggerMiddleware } from './items/middlewares/logger.middleware';
 @Module({
   imports: [ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('items');
+  }
+}
